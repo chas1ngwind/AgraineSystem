efb35dafcc8ba7cb3a771aedc2f3b8c84923edbb
mac <siyingzhangcn@hotmail.com>
2019-05-17 21:23:22 +0200

    redomodel

--- vsdk/service_development/models/models.py
+++ vsdk/service_development/models/models.py
@@ -6,16 +6,7 @@ class RoleInfo(models.Model):
     def __str__(self):
         return self.role
 
-class DetailInfo(models.Model):
-    amount = models.IntegerField(null=True)
-    type = models.CharField(max_length=10)
-    generation = models.CharField(max_length=10)
-    name = models.CharField(max_length=10, null=True)
-    contactnumber = models.IntegerField(null=True)
-    role = models.ForeignKey(RoleInfo, on_delete=models.CASCADE, null=True)
 
-    def __int__(self):
-        return self.type
 class DetailInfo(models.Model):
     amount = models.IntegerField(null=True)
     type = models.CharField(max_length=10)
